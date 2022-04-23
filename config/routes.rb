02d4eba74ed3cefbed93876af8477275531959b5
urilixo <falecomelvio@hotmail.com>
2022-04-22 23:06:19 -0300

    Add post route to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root 'staticpage#home'
+  root 'static_pages#index'
+  post 'index' => 'static_pages#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

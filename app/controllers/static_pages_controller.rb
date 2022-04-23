61019d08c73d5ea6106ae50abe82c61890b7e14d
urilixo <falecomelvio@hotmail.com>
2022-04-22 23:05:55 -0300

    Add controller behaviour to get photos based on posted id

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,18 @@
 class StaticPagesController < ApplicationController
+  require 'flickr'
+
+  def index
+    flickr = Flickr.new ENV['flickr_key'], ENV['flickr_secret']
+    begin
+      @photos = flickr.photos.search user_id: params[:id]
+    rescue Flickr::FailedResponse
+      @photos = flickr.photos.getRecent
+    end
+  end
+
+  private
+
+  def static_pages_params
+    params.require(:static_pages).permit(:id)
+  end
 end
